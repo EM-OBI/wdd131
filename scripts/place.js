71001ab98b8f299c37c4f51cf7893f835d782fae
EM-OBI <bas23016@byui.edu>
2024-10-08 20:15:15 +0100

    Final commit hopefully

--- scripts/place.js
+++ scripts/place.js
@@ -13,3 +13,37 @@ currentYear.innerHTML = `<span>${currentDate.getFullYear()}</span>`;
 dateLastModif.innerHTML = `<span>Last Modified: ${getLastModDate}/${getLastModMonth}/${getLastModYear} ${getLastModHour}:${getLastModMinute}:${getLastModSecond}</span>`
 
 
+const getTemp = () => {
+    tempStr = (document.querySelector("#temperature").textContent);
+    tempFloat = tempStr.trim().split("℉");
+    return parseFloat(tempFloat[0]);
+}
+const getWind = () => {
+    windStr = (document.querySelector("#wind").textContent);
+    windFloat = windStr.trim().split(" ");
+    return parseFloat(windFloat[0]);
+} 
+
+let tempVal = getTemp();
+let windVal = getWind();
+
+const windChill = (temp, wind) => {
+    return 35.74 + 0.621 * temp - 35.75 * (wind ** 0.16) + 0.4275 * temp * (wind ** 0.16);
+}
+
+const windChillRound = windChill(tempVal, windVal).toFixed(2);
+
+if (tempVal <= 50 && windVal > 3) {
+    document.querySelector("#wind-chill").textContent = `${windChillRound}℉`;
+} else {
+    document.querySelector("#wind-chill").textContent = `N/A`;
+}
+
+
+
+
+
+// python
+// def wind_chill_calculator(temp, velocity):
+//     wind_chill = 35.74 + 0.621 * temp - 35.75 * (velocity ** 0.16) + 0.4275 * temp * (velocity ** 0.16)
+//     return wind_chill
