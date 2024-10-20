69cc1389743964a1e4f61f6a8fb6781546ab9ea8
EM-OBI <bas23016@byui.edu>
2024-10-20 21:31:15 +0100

    week 5 form commit

---
+++ scripts/form.js
@@ -0,0 +1,72 @@
+//standard footer
+let currentYear = document.querySelector("#currentYear");
+let dateLastModif = document.querySelector("#lastModified");
+const currentDate = new Date();
+let oLastModif = new Date(document.lastModified);
+const getLastModYear = oLastModif.getFullYear();
+const getLastModMonth = oLastModif.getMonth();
+const getLastModDate = oLastModif.getDate();
+const getLastModHour = oLastModif.getHours();
+const getLastModMinute = oLastModif.getMinutes();
+const getLastModSecond = oLastModif.getSeconds();
+
+currentYear.innerHTML = `<span>${currentDate.getFullYear()}</span>`;
+dateLastModif.innerHTML = `<span>Last Modified: ${getLastModDate}/${getLastModMonth}/${getLastModYear} ${getLastModHour}:${getLastModMinute}:${getLastModSecond}</span>`
+
+//create options and populate product name
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+const productName = document.querySelector("#product-name");
+populateProducts(products);
+
+function populateProducts(products) {
+    products.forEach(product => {
+        let option = document.createElement("option");
+        option.textContent = product.name;
+        option.setAttribute("value", product.id);
+        productName.appendChild(option);
+    });
+}
+
+// localStorage
+const reviewCountDisplay = document.querySelector(".review-counter");
+
+let submissions = Number(window.localStorage.getItem("submissions-count")) || 0;
+
+    if (submissions === 0) {
+        reviewCountDisplay.textContent = "Welcome, please leave a review";
+    } else {
+        reviewCountDisplay.textContent = `Products reviewed: ${submissions}`;
+    }
+
+document.querySelector("input[type='submit']").addEventListener("click", () => {
+    submissions++;
+
+    localStorage.setItem("submissions-count", submissions);
+});
